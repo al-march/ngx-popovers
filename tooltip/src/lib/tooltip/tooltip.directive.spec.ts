923ca0d2ce65bd43868a7f1deb5e08262d713912
Alexander <valverin95@gmail.com>
2023-12-16 14:05:23 +0300

    add tooltip component

---
+++ tooltip/src/lib/tooltip/tooltip.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgxTooltip } from './tooltip.directive';
+
+describe('TooltipDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgxTooltip();
+    expect(directive).toBeTruthy();
+  });
+});
