d0589aa12af786772477d9dd1aa83b4f3012bf3d
Alexander <valverin95@gmail.com>
2023-12-25 15:45:44 +0300

    feat: [portal] bindTo any element in the Document

--- packages/portal/src/lib/portal/portal.component.spec.ts
+++ packages/portal/src/lib/portal/portal.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PortalComponent } from './portal.component';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 const testContentId = 'test-id';
 
@@ -18,16 +18,48 @@ describe('PortalComponent', () => {
     fixture.detectChanges();
   });
 
+
+  const parentId = 'parent-id';
+
+  const createParent = () => {
+    const parent = document.createElement('div');
+    parent.id = parentId;
+    document.body.appendChild(parent);
+    return parent;
+  };
+
+  const content = () => document.querySelector(`#${testContentId}`) as HTMLElement;
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render content into body', () => {
-    expect(fixture.nativeElement).not.toHaveTextContent(testContentId)
+  it('should render content into body by default', () => {
+    expect(fixture.nativeElement).not.toHaveTextContent(testContentId);
 
-    const component = document.querySelector(`#${testContentId}`);
-    expect(component).toBeInTheDocument();
-    expect(component).toHaveTextContent(testContentId);
+    expect(content()).toBeInTheDocument();
+    expect(content()).toHaveTextContent(testContentId);
+  });
+
+  it('should render content by bindTo (HTMLElement)', () => {
+    const parent = createParent();
+    fixture.componentRef.setInput('bindTo', parent);
+    fixture.detectChanges();
+
+    // Should be rendered into parent
+    expect(content()).toBeInTheDocument();
+    expect(content().parentElement?.id).toBe(parent.id);
+  });
+
+  it('should render content by bindTo (querySelector)', () => {
+    const parent = createParent();
+    fixture.componentRef.setInput('bindTo', `#${parentId}`);
+    fixture.detectChanges();
+
+    // Should be rendered into parent
+    expect(content()).toBeInTheDocument();
+    expect(content().parentElement?.id).toBe(parent.id);
   });
 });
 
@@ -37,9 +69,12 @@ describe('PortalComponent', () => {
     PortalComponent
   ],
   template: `
-      <ngx-portal><div [id]="testId">{{testId}}</div></ngx-portal>
+      <ngx-portal [bindTo]="bindTo"><div [id]="testId">{{ testId }}</div></ngx-portal>
   `
 })
 class PortalComponentTest {
   testId = testContentId;
+
+  @Input()
+  bindTo?: HTMLElement | string;
 }
--- packages/portal/src/lib/portal/portal.component.ts
+++ packages/portal/src/lib/portal/portal.component.ts
@@ -3,12 +3,14 @@ import {
   Component,
   EmbeddedViewRef,
   Inject,
+  Input,
+  OnChanges,
   OnDestroy,
   OnInit,
   Renderer2,
   TemplateRef,
   ViewChild,
-  ViewContainerRef,
+  ViewContainerRef
 } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 
@@ -18,14 +20,35 @@ import { CommonModule, DOCUMENT } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PortalComponent implements OnInit, OnDestroy {
+export class PortalComponent implements OnInit, OnChanges, OnDestroy {
   @ViewChild(TemplateRef, { static: true })
   portalContent?: TemplateRef<HTMLElement>;
 
+  @Input()
+  bindTo?: HTMLElement | string;
+
   private view?: EmbeddedViewRef<HTMLElement>;
   private panelRef?: HTMLElement;
+
+  get parent() {
+    if (this.bindTo instanceof HTMLElement) {
+      return this.bindTo;
+    }
+    if (typeof this.bindTo === 'string') {
+      return this.parentBySelector(this.bindTo) ?? this.document.body;
+    }
+    return this.document.body;
+  }
+
+  parentBySelector(selector: string) {
+    const element = this.document.querySelector(selector);
+    if (!element) {
+      console.error(`cannot find HTMLElement with query selector: ${selector}`);
+    }
+    return element;
+  }
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -34,11 +57,19 @@ export class PortalComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
+    this.bind();
+  }
+
+  ngOnChanges() {
+    this.bind();
+  }
+
+  bind() {
     if (this.portalContent) {
       this.view = this.viewContainerRef.createEmbeddedView(this.portalContent);
       this.panelRef = this.view.rootNodes[0];
       if (this.panelRef) {
-        this.renderer.appendChild(this.document.body, this.panelRef);
+        this.renderer.appendChild(this.parent, this.panelRef);
       } else {
         console.error('cannot render component into body');
       }
