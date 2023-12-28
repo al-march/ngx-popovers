9ef4381ef9dd21251a3f241608be0e5a91a87a57
Alexander <valverin95@gmail.com>
2023-12-29 00:42:40 +0300

    feat: [demo] portal docs

--- apps/ngx-popovers/src/app/pages/page-core/page-core.component.ts
+++ apps/ngx-popovers/src/app/pages/page-core/page-core.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from '../components/title/title.component';
-import { CoreIcon } from '../../routes-icons';
+import { CoreIcon, FloatingIcon, PortalIcon } from '../../routes-icons';
 import { RouterLink } from '@angular/router';
+import { HighlightComponent } from '../../core/highlight/highlight.component';
 
 @Component({
   selector: 'ngx-popovers-page-core',
   standalone: true,
-  imports: [CommonModule, TitleComponent, CoreIcon, RouterLink],
+  imports: [CommonModule, TitleComponent, CoreIcon, RouterLink, HighlightComponent, PortalIcon, FloatingIcon],
   templateUrl: './page-core.component.html',
-  styleUrl: './page-core.component.scss'
+  styleUrl: './page-core.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { 'ngSkipHydration': 'true' }
 })
 export class PageCoreComponent {
 }
--- apps/ngx-popovers/src/app/pages/page-core/page-portal/page-portal.component.ts
+++ apps/ngx-popovers/src/app/pages/page-core/page-portal/page-portal.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from '../../components/title/title.component';
+import { HighlightComponent } from '../../../core/highlight/highlight.component';
+
+
+const portalUsageHtml =
+  `
+<ngx-portal>
+  <p>It will be displayed as the last child of the body</p>
+</ngx-portal>
+`.trim();
+
+const portalUsageWithBindHtml =
+  `
+<div #element>
+  <p>Element</p>
+</div>
+
+<ngx-portal [bindTo]="element">
+  <p>It will be displayed as the last child of the element</p>
+</ngx-portal>
+`.trim();
 
 @Component({
   selector: 'ngx-popovers-page-portal',
   standalone: true,
-  imports: [CommonModule, TitleComponent],
+  imports: [CommonModule, TitleComponent, HighlightComponent],
   templateUrl: './page-portal.component.html',
   styleUrl: './page-portal.component.scss'
 })
 export class PagePortalComponent {
+  portalUsageHtml = portalUsageHtml;
+  portalUsageWithBindHtml = portalUsageWithBindHtml;
+
+  constructor() {
+  }
+
 }
