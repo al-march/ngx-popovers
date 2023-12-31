783b2a8a7f5ff7228dbbf9a33486f17bb4704ca6
Alexander <valverin95@gmail.com>
2023-12-31 15:41:16 +0300

    feat: [demo] auto routes

--- apps/ngx-popovers/src/app/template/header/header.component.ts
+++ apps/ngx-popovers/src/app/template/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '../../core/theme.service';
+import { ActivationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'dm-header',
@@ -10,13 +11,33 @@ import { ThemeService } from '../../core/theme.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  router = inject(Router);
+  paths = signal<string[]>([]);
 
   @Output()
   toggleSidebar = new EventEmitter();
 
   constructor(
     public themeService: ThemeService
-  ) {}
+  ) {
+    this.parsePaths();
+  }
+
+  parsePaths() {
+    this.router.events
+      .subscribe(event => {
+        if (event instanceof ActivationStart) {
+          const paths: string[] = [];
+          event.snapshot.pathFromRoot.map(path => {
+            const p = path.routeConfig?.path;
+            if (p) {
+              paths.push(p);
+            }
+          });
+          this.paths.set(paths);
+        }
+      });
+  }
 
   onToggleSidebar() {
     this.toggleSidebar.emit();
@@ -24,5 +45,10 @@ export class HeaderComponent {
 
   toggleTheme() {
     this.themeService.toggle();
+  }
+
+  navigateToPath($index: number) {
+    const paths = this.paths().slice(0, $index);
+    return this.router.navigate(['/', ...paths])
   }
 }
