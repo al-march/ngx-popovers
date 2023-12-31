01b4f218f903de1cbfc3b1dc5384ef7c4b9a2e1b
Alexander <valverin95@gmail.com>
2023-12-31 14:58:14 +0300

    feat: [demo] copy code examples

--- apps/ngx-popovers/src/app/core/highlight/highlight.component.ts
+++ apps/ngx-popovers/src/app/core/highlight/highlight.component.ts
@@ -1,6 +1,27 @@
-import { ChangeDetectionStrategy, Component, inject, Input, OnChanges, PLATFORM_ID, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  Input,
+  OnChanges,
+  PLATFORM_ID,
+  signal
+} from '@angular/core';
 import { CommonModule, isPlatformServer } from '@angular/common';
+import { Clipboard } from '@angular/cdk/clipboard';
 import hljs from 'highlight.js/lib/core';
+
+enum CopyStatus {
+  WAIT,
+  SUCCESS,
+  ERROR
+}
+
+type CopyButton = {
+  text: string;
+  status: CopyStatus;
+}
 
 @Component({
   selector: 'highlight',
@@ -24,6 +45,25 @@ export class HighlightComponent implements OnChanges {
   lang = '';
 
   markup = signal('');
+  copying = signal<CopyStatus>(CopyStatus.WAIT);
+
+  copyStatus = computed<CopyButton>(() => {
+    const status = this.copying();
+    switch (status) {
+      case CopyStatus.SUCCESS:
+        return { text: 'Copied', status };
+      case CopyStatus.ERROR:
+        return { text: 'Error', status };
+      default:
+        return { text: 'Copy', status };
+    }
+  });
+
+  statuses = CopyStatus;
+
+  constructor(
+    private clipboard: Clipboard
+  ) {}
 
   ngOnChanges() {
     if (!this.isServer) {
@@ -33,6 +73,25 @@ export class HighlightComponent implements OnChanges {
       ).value;
 
       this.markup.set(highlightedCode);
+    }
+  }
+
+  copy() {
+    const status = this.clipboard.copy(this.code);
+    this.setCopyStatus(status);
+  }
+
+  setCopyStatus(success: boolean) {
+    if (success) {
+      this.copying.set(CopyStatus.SUCCESS);
+      setTimeout(() => {
+        this.copying.set(CopyStatus.WAIT);
+      }, 1500);
+    } else {
+      this.copying.set(CopyStatus.ERROR);
+      setTimeout(() => {
+        this.copying.set(CopyStatus.WAIT);
+      }, 1500);
     }
   }
 }
