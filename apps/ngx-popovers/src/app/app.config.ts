55fd30ef1e5954c4e8057840234ab575c8a718b7
Alexander <valverin95@gmail.com>
2024-01-05 17:14:17 +0300

    fix: [demo] withInMemoryScrolling

--- apps/ngx-popovers/src/app/app.config.ts
+++ apps/ngx-popovers/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -7,7 +7,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withInMemoryScrolling({
+      scrollPositionRestoration: 'enabled'
+    })),
     provideAnimationsAsync()
   ]
 };
