6f3ba0c65e685dfc22ad7effbc91df827c5658ab
Alexander <valverin95@gmail.com>
2024-01-09 22:28:18 +0300

    feat: [demo] add footer component

---
+++ apps/ngx-popovers/src/app/template/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxTooltip } from '@ngx-popovers/tooltip';
+
+@Component({
+  selector: 'dm-footer',
+  standalone: true,
+  imports: [CommonModule, NgxTooltip],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterComponent {
+}
