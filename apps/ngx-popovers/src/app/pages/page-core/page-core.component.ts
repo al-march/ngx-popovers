f2b73a2b2e792529f08f54fae8c2dc88e943b328
Alexander <valverin95@gmail.com>
2024-01-15 22:32:56 +0300

    feat: [demo] add width & height inputs for arrows

--- apps/ngx-popovers/src/app/pages/page-core/page-core.component.ts
+++ apps/ngx-popovers/src/app/pages/page-core/page-core.component.ts
@@ -1,14 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from '../components/title/title.component';
-import { ClickOutsideIcon, CoreIcon, FloatingIcon, PortalIcon } from '../../routes-icons';
+import { ArrowIcon, ClickOutsideIcon, CoreIcon, FloatingIcon, PortalIcon } from '../../routes-icons';
 import { RouterLink } from '@angular/router';
 import { HighlightComponent } from '../../core/highlight/highlight.component';
 
 @Component({
   selector: 'ngx-popovers-page-core',
   standalone: true,
-  imports: [CommonModule, TitleComponent, CoreIcon, RouterLink, HighlightComponent, PortalIcon, FloatingIcon, ClickOutsideIcon],
+  imports: [CommonModule, TitleComponent, CoreIcon, RouterLink, HighlightComponent, PortalIcon, FloatingIcon, ClickOutsideIcon, ArrowIcon],
   templateUrl: './page-core.component.html',
   styleUrl: './page-core.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- apps/ngx-popovers/src/app/routes-icons.ts
+++ apps/ngx-popovers/src/app/routes-icons.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Directive, Input } from '@angular/core';
+
+@Directive()
+class IconComponentBase {
+  @Input()
+  width = 32;
+
+  @Input()
+  height = 32;
+}
 
 @Component({
   template: `
@@ -6,15 +15,15 @@ import { Component } from '@angular/core';
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
+      [style.width]="width"
+      [style.height]="height"
     ><path
       fill="currentColor"
       d="M8.953 17.985A2.5 2.5 0 016.5 20h-2a.5.5 0 01-.5-.5v-2a2.5 2.5 0 012.015-2.453l.012-.343c.02-.58.08-1.148.177-1.704H2.5a.5.5 0 01-.464-.686l2-5A.5.5 0 014.5 7H9c.025 0 .05.002.075.006A12.302 12.302 0 0120.59 3.008a.5.5 0 01.4.401 12.32 12.32 0 01-3.996 11.518A.504.504 0 0117 15v4.5a.5.5 0 01-.314.464l-5 2A.5.5 0 0111 21.5v-3.705c-.563.099-1.133.158-1.704.178l-.343.012zm-.966-.29l-1.682-1.682A1.5 1.5 0 005 17.5V19h1.5a1.5 1.5 0 001.487-1.305zM12 17.577v3.184l4-1.6v-3.448a12.254 12.254 0 01-4 1.864zM6.42 12A12.26 12.26 0 018.29 8h-3.45l-1.6 4H6.42zm2.28 4.993l.561-.02c.562-.019 1.12-.08 1.673-.182 6.036-1.118 10.062-6.831 9.132-12.857-.44-.068-.884-.11-1.33-.125-6.251-.216-11.494 4.678-11.71 10.93l-.019.561L8.7 16.993zM14 13a3 3 0 110-6 3 3 0 010 6zm0-1a2 2 0 100-4 2 2 0 000 4z"
     ></path></svg>`,
   standalone: true
 })
-export class GettingStartedIcon {
+export class GettingStartedIcon extends IconComponentBase {
 }
 
 
@@ -24,16 +33,19 @@ export class GettingStartedIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path
-      fill="currentColor"
-      d="M21.987 13A9.5 9.5 0 0113 21.987V23.5a.5.5 0 11-1 0v-1.513A9.5 9.5 0 013.013 13H1.5a.5.5 0 110-1h1.513A9.5 9.5 0 0112 3.013V1.5a.5.5 0 111 0v1.513A9.5 9.5 0 0121.987 12H23.5a.5.5 0 110 1h-1.513zM12.5 21a8.5 8.5 0 100-17 8.5 8.5 0 000 17zm0-4a4.5 4.5 0 110-9 4.5 4.5 0 010 9zm0-1a3.5 3.5 0 100-7 3.5 3.5 0 000 7z"
-    ></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        fill="currentColor"
+        d="M21.987 13A9.5 9.5 0 0113 21.987V23.5a.5.5 0 11-1 0v-1.513A9.5 9.5 0 013.013 13H1.5a.5.5 0 110-1h1.513A9.5 9.5 0 0112 3.013V1.5a.5.5 0 111 0v1.513A9.5 9.5 0 0121.987 12H23.5a.5.5 0 110 1h-1.513zM12.5 21a8.5 8.5 0 100-17 8.5 8.5 0 000 17zm0-4a4.5 4.5 0 110-9 4.5 4.5 0 010 9zm0-1a3.5 3.5 0 100-7 3.5 3.5 0 000 7z"
+      ></path>
+    </svg>
+  `,
   selector: 'core-icon',
   standalone: true
 })
-export class CoreIcon {
+export class CoreIcon extends IconComponentBase {
 }
 
 @Component({
@@ -42,16 +54,19 @@ export class CoreIcon {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path
-      fill="currentColor"
-      d="M18.285 17.578l2.569 2.568a.5.5 0 01-.708.708l-2.568-2.57-1.662 2.493a.5.5 0 01-.884-.101l-3-8a.5.5 0 01.644-.644l8 3a.5.5 0 01.101.884l-2.492 1.662zm-1.054-.5l2.18-1.453-6.057-2.27 2.271 6.055 1.453-2.179a.502.502 0 01.153-.153zM5.5 3a.5.5 0 010 1A1.5 1.5 0 004 5.5a.5.5 0 01-1 0A2.5 2.5 0 015.5 3zm3 1a.5.5 0 010-1h2a.5.5 0 110 1h-2zm5 0a.5.5 0 110-1h2a.5.5 0 110 1h-2zm-5 17a.5.5 0 110-1h2a.5.5 0 110 1h-2zM3 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 005.5 20a.5.5 0 110 1A2.5 2.5 0 013 18.5zm18-8a.5.5 0 11-1 0v-2a.5.5 0 111 0v2zm0-5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1A2.5 2.5 0 0121 5.5z"
-    ></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        fill="currentColor"
+        d="M18.285 17.578l2.569 2.568a.5.5 0 01-.708.708l-2.568-2.57-1.662 2.493a.5.5 0 01-.884-.101l-3-8a.5.5 0 01.644-.644l8 3a.5.5 0 01.101.884l-2.492 1.662zm-1.054-.5l2.18-1.453-6.057-2.27 2.271 6.055 1.453-2.179a.502.502 0 01.153-.153zM5.5 3a.5.5 0 010 1A1.5 1.5 0 004 5.5a.5.5 0 01-1 0A2.5 2.5 0 015.5 3zm3 1a.5.5 0 010-1h2a.5.5 0 110 1h-2zm5 0a.5.5 0 110-1h2a.5.5 0 110 1h-2zm-5 17a.5.5 0 110-1h2a.5.5 0 110 1h-2zM3 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 005.5 20a.5.5 0 110 1A2.5 2.5 0 013 18.5zm18-8a.5.5 0 11-1 0v-2a.5.5 0 111 0v2zm0-5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1A2.5 2.5 0 0121 5.5z"
+      ></path>
+    </svg>
+  `,
   selector: 'tooltip-icon',
   standalone: true
 })
-export class TooltipIcon {
+export class TooltipIcon extends IconComponentBase {
 }
 
 @Component({
@@ -60,16 +75,19 @@ export class TooltipIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path
-      fill="currentColor"
-      d="M14.293 12H6.5a.5.5 0 110-1h7.793l-2.147-2.146a.5.5 0 01.708-.708l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L14.293 12zM4.5 3a.5.5 0 010 1A1.5 1.5 0 003 5.5a.5.5 0 01-1 0A2.5 2.5 0 014.5 3zM2 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 004.5 20a.5.5 0 110 1A2.5 2.5 0 012 18.5zm8-13a.5.5 0 01-1 0A2.5 2.5 0 0111.5 3h8A2.5 2.5 0 0122 5.5v13a2.5 2.5 0 01-2.5 2.5h-8A2.5 2.5 0 019 18.5a.5.5 0 111 0 1.5 1.5 0 001.5 1.5h8a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0019.5 4h-8A1.5 1.5 0 0010 5.5zM8.5 20a.5.5 0 110 1h-2a.5.5 0 110-1h2zm0-17a.5.5 0 010 1h-2a.5.5 0 010-1h2z"
-    ></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        fill="currentColor"
+        d="M14.293 12H6.5a.5.5 0 110-1h7.793l-2.147-2.146a.5.5 0 01.708-.708l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L14.293 12zM4.5 3a.5.5 0 010 1A1.5 1.5 0 003 5.5a.5.5 0 01-1 0A2.5 2.5 0 014.5 3zM2 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 004.5 20a.5.5 0 110 1A2.5 2.5 0 012 18.5zm8-13a.5.5 0 01-1 0A2.5 2.5 0 0111.5 3h8A2.5 2.5 0 0122 5.5v13a2.5 2.5 0 01-2.5 2.5h-8A2.5 2.5 0 019 18.5a.5.5 0 111 0 1.5 1.5 0 001.5 1.5h8a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0019.5 4h-8A1.5 1.5 0 0010 5.5zM8.5 20a.5.5 0 110 1h-2a.5.5 0 110-1h2zm0-17a.5.5 0 010 1h-2a.5.5 0 010-1h2z"
+      ></path>
+    </svg>
+  `,
   selector: 'portal-icon',
   standalone: true
 })
-export class PortalIcon {
+export class PortalIcon extends IconComponentBase {
 }
 
 @Component({
@@ -78,8 +96,8 @@ export class PortalIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
+      [style.width]="width"
+      [style.height]="height"
     ><path
       fill="currentColor"
       d="M13 12h.5a.5.5 0 110 1H13v.5a.5.5 0 11-1 0V13h-.5a.5.5 0 110-1h.5v-.5a.5.5 0 111 0v.5zM3 5.5A2.5 2.5 0 015.5 3h14A2.5 2.5 0 0122 5.5v14.007a2.5 2.5 0 01-2.5 2.5h-14a2.5 2.5 0 01-2.5-2.5V5.5zm1 0v14.007a1.5 1.5 0 001.5 1.5h14a1.5 1.5 0 001.5-1.5V5.5A1.5 1.5 0 0019.5 4h-14A1.5 1.5 0 004 5.5zM15.5 13a.5.5 0 110-1h1a.5.5 0 110 1h-1zm3 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zm-13 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zm3 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zM12 5.5a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0 3a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0 10a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0-3a.5.5 0 111 0v1a.5.5 0 11-1 0v-1z"
@@ -87,7 +105,7 @@ export class PortalIcon {
   selector: 'floating-icon',
   standalone: true
 })
-export class FloatingIcon {
+export class FloatingIcon extends IconComponentBase {
 }
 
 
@@ -97,8 +115,8 @@ export class FloatingIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
+      [style.width]="width"
+      [style.height]="height"
     ><path
       fill="currentColor"
       d="M8.854 19.854a.5.5 0 01-.831-.205l-5-16a.5.5 0 01.626-.626l16 5a.5.5 0 01.205.83L16.207 12.5l4.647 4.646a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0L12.5 16.207l-3.646 3.647zM4.262 4.262l4.469 14.3 3.415-3.416a.5.5 0 01.708 0l4.646 4.647 2.293-2.293-4.647-4.646a.5.5 0 010-.708l3.416-3.415-14.3-4.469z"
@@ -106,7 +124,7 @@ export class FloatingIcon {
   selector: 'click-outside-icon',
   standalone: true
 })
-export class ClickOutsideIcon {
+export class ClickOutsideIcon extends IconComponentBase {
 }
 
 @Component({
@@ -115,8 +133,8 @@ export class ClickOutsideIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
+      [style.width]="width"
+      [style.height]="height"
     ><path
       fill="currentColor"
       d="M20 11.5v-4A3.5 3.5 0 0016.5 4h-9A3.5 3.5 0 004 7.5v9A3.5 3.5 0 007.5 20h5a1.5 1.5 0 001.5-1.5v-2a3.5 3.5 0 013.5-3.5h1a1.5 1.5 0 001.5-1.5zm-.073 2.053A2.488 2.488 0 0118.5 14h-1a2.5 2.5 0 00-2.5 2.5v2c0 .422-.105.82-.29 1.169a7.509 7.509 0 005.217-6.116zM7.5 3h9A4.5 4.5 0 0121 7.5v5a8.5 8.5 0 01-8.5 8.5h-5A4.5 4.5 0 013 16.5v-9A4.5 4.5 0 017.5 3z"
@@ -124,7 +142,7 @@ export class ClickOutsideIcon {
   selector: 'popover-icon',
   standalone: true
 })
-export class PopoverIcon {
+export class PopoverIcon extends IconComponentBase {
 }
 
 @Component({
@@ -133,8 +151,8 @@ export class PopoverIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
+      [style.width]="width"
+      [style.height]="height"
     >
       <path
         fill="currentColor"
@@ -145,7 +163,7 @@ export class PopoverIcon {
   selector: 'arrow-icon',
   standalone: true
 })
-export class ArrowIcon {
+export class ArrowIcon extends IconComponentBase {
 }
 
 @Component({
@@ -155,12 +173,16 @@ export class ArrowIcon {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path d="M4.5 9A1.5 1.5 0 003 10.5v9A1.5 1.5 0 004.5 21h9.01a1.5 1.5 0 001.5-1.5v-9a1.5 1.5 0 00-1.5-1.5H4.5zM9 8h4.51a2.5 2.5 0 012.5 2.5V15h.49a.5.5 0 110 1h-.49v3.5a2.5 2.5 0 01-2.5 2.5H4.5A2.5 2.5 0 012 19.5v-9A2.5 2.5 0 014.5 8H8v-.5a.5.5 0 011 0V8zm1.5-6a.5.5 0 110 1A1.5 1.5 0 009 4.5a.5.5 0 01-1 0A2.5 2.5 0 0110.5 2zm9 1a.5.5 0 110-1A2.5 2.5 0 0122 4.5a.5.5 0 11-1 0A1.5 1.5 0 0019.5 3zM21 7.5a.5.5 0 111 0v3a.5.5 0 11-1 0v-3zm0 6a.5.5 0 111 0 2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5zM16.5 2a.5.5 0 110 1h-3a.5.5 0 110-1h3z"></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        d="M4.5 9A1.5 1.5 0 003 10.5v9A1.5 1.5 0 004.5 21h9.01a1.5 1.5 0 001.5-1.5v-9a1.5 1.5 0 00-1.5-1.5H4.5zM9 8h4.51a2.5 2.5 0 012.5 2.5V15h.49a.5.5 0 110 1h-.49v3.5a2.5 2.5 0 01-2.5 2.5H4.5A2.5 2.5 0 012 19.5v-9A2.5 2.5 0 014.5 8H8v-.5a.5.5 0 011 0V8zm1.5-6a.5.5 0 110 1A1.5 1.5 0 009 4.5a.5.5 0 01-1 0A2.5 2.5 0 0110.5 2zm9 1a.5.5 0 110-1A2.5 2.5 0 0122 4.5a.5.5 0 11-1 0A1.5 1.5 0 0019.5 3zM21 7.5a.5.5 0 111 0v3a.5.5 0 11-1 0v-3zm0 6a.5.5 0 111 0 2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5zM16.5 2a.5.5 0 110 1h-3a.5.5 0 110-1h3z"
+      ></path>
+    </svg>`,
   standalone: true
 })
-export class OffsetIcon {
+export class OffsetIcon extends IconComponentBase {
 }
 
 
@@ -170,15 +192,18 @@ export class OffsetIcon {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path
-      fill="currentColor"
-      d="M4.5 3a.5.5 0 010 1A1.5 1.5 0 003 5.5a.5.5 0 01-1 0A2.5 2.5 0 014.5 3zM2 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 004.5 20a.5.5 0 110 1A2.5 2.5 0 012 18.5zm7-13A2.5 2.5 0 0111.5 3h8A2.5 2.5 0 0122 5.5v13a2.5 2.5 0 01-2.5 2.5h-8A2.5 2.5 0 019 18.5v-13zm1 0v13a1.5 1.5 0 001.5 1.5h8a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0019.5 4h-8A1.5 1.5 0 0010 5.5zM8.5 20a.5.5 0 110 1h-2a.5.5 0 110-1h2zm0-17a.5.5 0 010 1h-2a.5.5 0 010-1h2z"
-    ></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        fill="currentColor"
+        d="M4.5 3a.5.5 0 010 1A1.5 1.5 0 003 5.5a.5.5 0 01-1 0A2.5 2.5 0 014.5 3zM2 8.5a.5.5 0 011 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0v2a.5.5 0 11-1 0v-2zm0 5a.5.5 0 111 0A1.5 1.5 0 004.5 20a.5.5 0 110 1A2.5 2.5 0 012 18.5zm7-13A2.5 2.5 0 0111.5 3h8A2.5 2.5 0 0122 5.5v13a2.5 2.5 0 01-2.5 2.5h-8A2.5 2.5 0 019 18.5v-13zm1 0v13a1.5 1.5 0 001.5 1.5h8a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0019.5 4h-8A1.5 1.5 0 0010 5.5zM8.5 20a.5.5 0 110 1h-2a.5.5 0 110-1h2zm0-17a.5.5 0 010 1h-2a.5.5 0 010-1h2z"
+      ></path>
+    </svg>
+  `,
   standalone: true
 })
-export class ShiftIcon {
+export class ShiftIcon extends IconComponentBase {
 }
 
 
@@ -188,14 +213,15 @@ export class ShiftIcon {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      width="32"
-      height="32"
-    ><path
-      fill="currentColor"
-      d="M16.053 12.724C15.135 10.887 13.803 10 12 10c-1.804 0-3.135.887-4.053 2.724a.5.5 0 01-.894-.448C8.135 10.113 9.803 9 12 9c2.196 0 3.865 1.113 4.947 3.276a.5.5 0 01-.894.448zM16 12V9.5a.5.5 0 111 0v3a.5.5 0 01-.5.5h-3a.5.5 0 110-1H16zm2.5-9A2.5 2.5 0 0121 5.5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1zm-2 1h-2a.5.5 0 110-1h2a.5.5 0 110 1zM21 8.5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5a2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5.5.5 0 111 0zM16.5 21h-2a.5.5 0 110-1h2a.5.5 0 110 1zM12 2.5v4a.5.5 0 11-1 0v-4a.5.5 0 111 0zm0 12v7a.5.5 0 11-1 0v-7a.5.5 0 111 0zM8.5 4h-3A1.5 1.5 0 004 5.5v13A1.5 1.5 0 005.5 20h3a.5.5 0 110 1h-3A2.5 2.5 0 013 18.5v-13A2.5 2.5 0 015.5 3h3a.5.5 0 010 1z"
-    ></path></svg>`,
+      [style.width]="width"
+      [style.height]="height"
+    >
+      <path
+        fill="currentColor"
+        d="M16.053 12.724C15.135 10.887 13.803 10 12 10c-1.804 0-3.135.887-4.053 2.724a.5.5 0 01-.894-.448C8.135 10.113 9.803 9 12 9c2.196 0 3.865 1.113 4.947 3.276a.5.5 0 01-.894.448zM16 12V9.5a.5.5 0 111 0v3a.5.5 0 01-.5.5h-3a.5.5 0 110-1H16zm2.5-9A2.5 2.5 0 0121 5.5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1zm-2 1h-2a.5.5 0 110-1h2a.5.5 0 110 1zM21 8.5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5a2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5.5.5 0 111 0zM16.5 21h-2a.5.5 0 110-1h2a.5.5 0 110 1zM12 2.5v4a.5.5 0 11-1 0v-4a.5.5 0 111 0zm0 12v7a.5.5 0 11-1 0v-7a.5.5 0 111 0zM8.5 4h-3A1.5 1.5 0 004 5.5v13A1.5 1.5 0 005.5 20h3a.5.5 0 110 1h-3A2.5 2.5 0 013 18.5v-13A2.5 2.5 0 015.5 3h3a.5.5 0 010 1z"
+      ></path>
+    </svg>`,
   standalone: true
 })
-export class FlipIcon {
+export class FlipIcon extends IconComponentBase {
 }
-
