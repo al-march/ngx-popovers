91c0eedd8080eaa53b9bf57272bff412a6549931
Alexander <valverin95@gmail.com>
2024-02-03 02:42:48 +0300

    feat: [DEMO] add link to the dialog + update code examples

--- apps/ngx-popovers/src/app/pages/page-getting-started/page-getting-started.component.ts
+++ apps/ngx-popovers/src/app/pages/page-getting-started/page-getting-started.component.ts
@@ -1,15 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from '../components/title/title.component';
-import { Placement } from '@floating-ui/dom';
 import { Arrow, FloatingComponent } from '@ngx-popovers/core';
-import { CoreIcon, PopoverIcon, TooltipIcon } from '../../routes-icons';
+import { CoreIcon, DialogIcon, PopoverIcon, TooltipIcon } from '../../routes-icons';
 import { RouterLink } from '@angular/router';
+import { HighlightComponent } from '../../core/highlight/highlight.component';
 
 @Component({
   selector: 'ngx-popovers-page-getting-started',
   standalone: true,
-  imports: [CommonModule, TitleComponent, FloatingComponent, Arrow, PopoverIcon, TooltipIcon, CoreIcon, RouterLink],
+  imports: [
+    CommonModule,
+    TitleComponent,
+    FloatingComponent,
+    Arrow,
+    PopoverIcon,
+    TooltipIcon,
+    CoreIcon,
+    RouterLink,
+    DialogIcon,
+    HighlightComponent
+  ],
   templateUrl: './page-getting-started.component.html',
   styleUrl: './page-getting-started.component.scss'
 })
