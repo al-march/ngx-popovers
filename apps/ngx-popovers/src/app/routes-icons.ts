34b8e56f37b67094d22f6c11cb5d3bd5d244d03f
Alexandr <42899726+al-march@users.noreply.github.com>
2024-02-03 10:31:18 +0300

    feat: [DEMO] add link to the dialog + update code examples (#25)

--- apps/ngx-popovers/src/app/routes-icons.ts
+++ apps/ngx-popovers/src/app/routes-icons.ts
@@ -2,31 +2,30 @@ import { Component, Directive, Input, numberAttribute } from '@angular/core';
 
 @Directive()
 class IconComponentBase {
-  @Input({transform: numberAttribute})
+  @Input({ transform: numberAttribute })
   width = 32;
 
-  @Input({transform: numberAttribute})
+  @Input({ transform: numberAttribute })
   height = 32;
 }
 
 @Component({
   selector: 'getting-started-icon',
-  template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    ><path
+  template: ` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
       fill="currentColor"
       d="M8.953 17.985A2.5 2.5 0 016.5 20h-2a.5.5 0 01-.5-.5v-2a2.5 2.5 0 012.015-2.453l.012-.343c.02-.58.08-1.148.177-1.704H2.5a.5.5 0 01-.464-.686l2-5A.5.5 0 014.5 7H9c.025 0 .05.002.075.006A12.302 12.302 0 0120.59 3.008a.5.5 0 01.4.401 12.32 12.32 0 01-3.996 11.518A.504.504 0 0117 15v4.5a.5.5 0 01-.314.464l-5 2A.5.5 0 0111 21.5v-3.705c-.563.099-1.133.158-1.704.178l-.343.012zm-.966-.29l-1.682-1.682A1.5 1.5 0 005 17.5V19h1.5a1.5 1.5 0 001.487-1.305zM12 17.577v3.184l4-1.6v-3.448a12.254 12.254 0 01-4 1.864zM6.42 12A12.26 12.26 0 018.29 8h-3.45l-1.6 4H6.42zm2.28 4.993l.561-.02c.562-.019 1.12-.08 1.673-.182 6.036-1.118 10.062-6.831 9.132-12.857-.44-.068-.884-.11-1.33-.125-6.251-.216-11.494 4.678-11.71 10.93l-.019.561L8.7 16.993zM14 13a3 3 0 110-6 3 3 0 010 6zm0-1a2 2 0 100-4 2 2 0 000 4z"
-    ></path></svg>`,
-  standalone: true
+    ></path>
+  </svg>`,
+  standalone: true,
 })
-export class GettingStartedIcon extends IconComponentBase {
-}
-
+export class GettingStartedIcon extends IconComponentBase {}
 
 @Component({
   template: `
@@ -44,10 +43,9 @@ export class GettingStartedIcon extends IconComponentBase {
     </svg>
   `,
   selector: 'core-icon',
-  standalone: true
+  standalone: true,
 })
-export class CoreIcon extends IconComponentBase {
-}
+export class CoreIcon extends IconComponentBase {}
 
 @Component({
   template: `
@@ -65,10 +63,9 @@ export class CoreIcon extends IconComponentBase {
     </svg>
   `,
   selector: 'tooltip-icon',
-  standalone: true
+  standalone: true,
 })
-export class TooltipIcon extends IconComponentBase {
-}
+export class TooltipIcon extends IconComponentBase {}
 
 @Component({
   template: `
@@ -86,65 +83,63 @@ export class TooltipIcon extends IconComponentBase {
     </svg>
   `,
   selector: 'portal-icon',
-  standalone: true
+  standalone: true,
 })
-export class PortalIcon extends IconComponentBase {
-}
+export class PortalIcon extends IconComponentBase {}
 
 @Component({
-  template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    ><path
+  template: ` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
       fill="currentColor"
       d="M13 12h.5a.5.5 0 110 1H13v.5a.5.5 0 11-1 0V13h-.5a.5.5 0 110-1h.5v-.5a.5.5 0 111 0v.5zM3 5.5A2.5 2.5 0 015.5 3h14A2.5 2.5 0 0122 5.5v14.007a2.5 2.5 0 01-2.5 2.5h-14a2.5 2.5 0 01-2.5-2.5V5.5zm1 0v14.007a1.5 1.5 0 001.5 1.5h14a1.5 1.5 0 001.5-1.5V5.5A1.5 1.5 0 0019.5 4h-14A1.5 1.5 0 004 5.5zM15.5 13a.5.5 0 110-1h1a.5.5 0 110 1h-1zm3 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zm-13 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zm3 0a.5.5 0 110-1h1a.5.5 0 110 1h-1zM12 5.5a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0 3a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0 10a.5.5 0 111 0v1a.5.5 0 11-1 0v-1zm0-3a.5.5 0 111 0v1a.5.5 0 11-1 0v-1z"
-    ></path></svg>`,
+    ></path>
+  </svg>`,
   selector: 'floating-icon',
-  standalone: true
+  standalone: true,
 })
-export class FloatingIcon extends IconComponentBase {
-}
-
+export class FloatingIcon extends IconComponentBase {}
 
 @Component({
-  template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    ><path
+  template: ` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
       fill="currentColor"
       d="M8.854 19.854a.5.5 0 01-.831-.205l-5-16a.5.5 0 01.626-.626l16 5a.5.5 0 01.205.83L16.207 12.5l4.647 4.646a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0L12.5 16.207l-3.646 3.647zM4.262 4.262l4.469 14.3 3.415-3.416a.5.5 0 01.708 0l4.646 4.647 2.293-2.293-4.647-4.646a.5.5 0 010-.708l3.416-3.415-14.3-4.469z"
-    ></path></svg>`,
+    ></path>
+  </svg>`,
   selector: 'click-outside-icon',
-  standalone: true
+  standalone: true,
 })
-export class ClickOutsideIcon extends IconComponentBase {
-}
+export class ClickOutsideIcon extends IconComponentBase {}
 
 @Component({
-  template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    ><path
+  template: ` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
       fill="currentColor"
       d="M20 11.5v-4A3.5 3.5 0 0016.5 4h-9A3.5 3.5 0 004 7.5v9A3.5 3.5 0 007.5 20h5a1.5 1.5 0 001.5-1.5v-2a3.5 3.5 0 013.5-3.5h1a1.5 1.5 0 001.5-1.5zm-.073 2.053A2.488 2.488 0 0118.5 14h-1a2.5 2.5 0 00-2.5 2.5v2c0 .422-.105.82-.29 1.169a7.509 7.509 0 005.217-6.116zM7.5 3h9A4.5 4.5 0 0121 7.5v5a8.5 8.5 0 01-8.5 8.5h-5A4.5 4.5 0 013 16.5v-9A4.5 4.5 0 017.5 3z"
-    ></path></svg>`,
+    ></path>
+  </svg>`,
   selector: 'popover-icon',
-  standalone: true
+  standalone: true,
 })
-export class PopoverIcon extends IconComponentBase {
-}
+export class PopoverIcon extends IconComponentBase {}
 
 @Component({
   template: `
@@ -162,30 +157,26 @@ export class PopoverIcon extends IconComponentBase {
     </svg>
   `,
   selector: 'arrow-icon',
-  standalone: true
+  standalone: true,
 })
-export class ArrowIcon extends IconComponentBase {
-}
+export class ArrowIcon extends IconComponentBase {}
 
 @Component({
-  template: `
-    <svg
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    >
-      <path
-        d="M4.5 9A1.5 1.5 0 003 10.5v9A1.5 1.5 0 004.5 21h9.01a1.5 1.5 0 001.5-1.5v-9a1.5 1.5 0 00-1.5-1.5H4.5zM9 8h4.51a2.5 2.5 0 012.5 2.5V15h.49a.5.5 0 110 1h-.49v3.5a2.5 2.5 0 01-2.5 2.5H4.5A2.5 2.5 0 012 19.5v-9A2.5 2.5 0 014.5 8H8v-.5a.5.5 0 011 0V8zm1.5-6a.5.5 0 110 1A1.5 1.5 0 009 4.5a.5.5 0 01-1 0A2.5 2.5 0 0110.5 2zm9 1a.5.5 0 110-1A2.5 2.5 0 0122 4.5a.5.5 0 11-1 0A1.5 1.5 0 0019.5 3zM21 7.5a.5.5 0 111 0v3a.5.5 0 11-1 0v-3zm0 6a.5.5 0 111 0 2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5zM16.5 2a.5.5 0 110 1h-3a.5.5 0 110-1h3z"
-      ></path>
-    </svg>`,
-  standalone: true
+  template: ` <svg
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
+      d="M4.5 9A1.5 1.5 0 003 10.5v9A1.5 1.5 0 004.5 21h9.01a1.5 1.5 0 001.5-1.5v-9a1.5 1.5 0 00-1.5-1.5H4.5zM9 8h4.51a2.5 2.5 0 012.5 2.5V15h.49a.5.5 0 110 1h-.49v3.5a2.5 2.5 0 01-2.5 2.5H4.5A2.5 2.5 0 012 19.5v-9A2.5 2.5 0 014.5 8H8v-.5a.5.5 0 011 0V8zm1.5-6a.5.5 0 110 1A1.5 1.5 0 009 4.5a.5.5 0 01-1 0A2.5 2.5 0 0110.5 2zm9 1a.5.5 0 110-1A2.5 2.5 0 0122 4.5a.5.5 0 11-1 0A1.5 1.5 0 0019.5 3zM21 7.5a.5.5 0 111 0v3a.5.5 0 11-1 0v-3zm0 6a.5.5 0 111 0 2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5zM16.5 2a.5.5 0 110 1h-3a.5.5 0 110-1h3z"
+    ></path>
+  </svg>`,
+  standalone: true,
 })
-export class OffsetIcon extends IconComponentBase {
-}
-
+export class OffsetIcon extends IconComponentBase {}
 
 @Component({
   template: `
@@ -202,27 +193,24 @@ export class OffsetIcon extends IconComponentBase {
       ></path>
     </svg>
   `,
-  standalone: true
+  selector: 'dialog-icon',
+  standalone: true,
 })
-export class DialogIcon extends IconComponentBase {
-}
-
+export class DialogIcon extends IconComponentBase {}
 
 @Component({
-  template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      [style.width]="width"
-      [style.height]="height"
-    >
-      <path
-        fill="currentColor"
-        d="M16.053 12.724C15.135 10.887 13.803 10 12 10c-1.804 0-3.135.887-4.053 2.724a.5.5 0 01-.894-.448C8.135 10.113 9.803 9 12 9c2.196 0 3.865 1.113 4.947 3.276a.5.5 0 01-.894.448zM16 12V9.5a.5.5 0 111 0v3a.5.5 0 01-.5.5h-3a.5.5 0 110-1H16zm2.5-9A2.5 2.5 0 0121 5.5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1zm-2 1h-2a.5.5 0 110-1h2a.5.5 0 110 1zM21 8.5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5a2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5.5.5 0 111 0zM16.5 21h-2a.5.5 0 110-1h2a.5.5 0 110 1zM12 2.5v4a.5.5 0 11-1 0v-4a.5.5 0 111 0zm0 12v7a.5.5 0 11-1 0v-7a.5.5 0 111 0zM8.5 4h-3A1.5 1.5 0 004 5.5v13A1.5 1.5 0 005.5 20h3a.5.5 0 110 1h-3A2.5 2.5 0 013 18.5v-13A2.5 2.5 0 015.5 3h3a.5.5 0 010 1z"
-      ></path>
-    </svg>`,
-  standalone: true
+  template: ` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    [style.width]="width"
+    [style.height]="height"
+  >
+    <path
+      fill="currentColor"
+      d="M16.053 12.724C15.135 10.887 13.803 10 12 10c-1.804 0-3.135.887-4.053 2.724a.5.5 0 01-.894-.448C8.135 10.113 9.803 9 12 9c2.196 0 3.865 1.113 4.947 3.276a.5.5 0 01-.894.448zM16 12V9.5a.5.5 0 111 0v3a.5.5 0 01-.5.5h-3a.5.5 0 110-1H16zm2.5-9A2.5 2.5 0 0121 5.5a.5.5 0 11-1 0A1.5 1.5 0 0018.5 4a.5.5 0 110-1zm-2 1h-2a.5.5 0 110-1h2a.5.5 0 110 1zM21 8.5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5v2a.5.5 0 11-1 0v-2a.5.5 0 111 0zm0 5a2.5 2.5 0 01-2.5 2.5.5.5 0 110-1 1.5 1.5 0 001.5-1.5.5.5 0 111 0zM16.5 21h-2a.5.5 0 110-1h2a.5.5 0 110 1zM12 2.5v4a.5.5 0 11-1 0v-4a.5.5 0 111 0zm0 12v7a.5.5 0 11-1 0v-7a.5.5 0 111 0zM8.5 4h-3A1.5 1.5 0 004 5.5v13A1.5 1.5 0 005.5 20h3a.5.5 0 110 1h-3A2.5 2.5 0 013 18.5v-13A2.5 2.5 0 015.5 3h3a.5.5 0 010 1z"
+    ></path>
+  </svg>`,
+  standalone: true,
 })
-export class FlipIcon extends IconComponentBase {
-}
+export class FlipIcon extends IconComponentBase {}
