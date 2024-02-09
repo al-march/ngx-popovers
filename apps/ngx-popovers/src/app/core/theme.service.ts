b77be7215d57fe29c64828040556f9ccec8e07f7
Alexandr <42899726+al-march@users.noreply.github.com>
2024-02-09 11:23:35 +0300

    feat: [DEMO] theme toggler (#34)

--- apps/ngx-popovers/src/app/core/theme.service.ts
+++ apps/ngx-popovers/src/app/core/theme.service.ts
@@ -1,6 +1,8 @@
 import { effect, Inject, Injectable, Renderer2, RendererFactory2, signal } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+const APP_THEME = 'APP_THEME';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +12,16 @@ export class ThemeService {
 
   theme = signal<'dark' | 'light'>('dark');
 
+  get localStorage() {
+    return this.doc.defaultView?.localStorage;
+  }
+
   constructor(
     @Inject(DOCUMENT) private doc: Document,
     rendererFactory: RendererFactory2
   ) {
     this.renderer = rendererFactory.createRenderer(null, null);
-    const theme = this.doc.documentElement.dataset?.['theme'];
+    const theme = this.getTheme();
     if (theme) {
       if (theme === 'dark' || theme === 'light') {
         this.theme.set(theme);
@@ -43,9 +49,48 @@ export class ThemeService {
 
   light() {
     this.renderer.setAttribute(this.doc.documentElement, 'data-theme', 'light');
+    this.saveTheme();
   }
 
   dark() {
     this.renderer.setAttribute(this.doc.documentElement, 'data-theme', 'dark');
+    this.saveTheme();
+  }
+
+  saveTheme() {
+    this.localStorage?.setItem(APP_THEME, this.theme());
+  }
+
+  getTheme(): 'dark' | 'light' {
+    try {
+      const theme = this.localStorage?.getItem(APP_THEME);
+      if (theme && this.isValidTheme(theme)) {
+        return theme;
+      } else {
+        return this.getSystemTheme();
+      }
+    } catch {
+      return this.getSystemTheme();
+    }
+  }
+
+  getSystemTheme() {
+    if (!this.doc.defaultView?.matchMedia) {
+      return 'dark';
+    }
+
+    const isLight = this.doc.defaultView
+      ?.matchMedia?.('(prefers-color-scheme: light)')
+      .matches;
+
+    if (isLight) {
+      return 'dark';
+    } else {
+      return 'light';
+    }
+  }
+
+  isValidTheme(str: unknown): str is 'dark' | 'light' {
+    return str === 'dark' || str === 'light';
   }
 }
