1784cc52d02c14533fd32b35b2fe75b641120687
Alexander <valverin95@gmail.com>
2024-03-19 02:33:15 +0300

    fix: [docs] destroy animation loop

--- apps/ngx-popovers/src/app/pages/landing/landing.component.ts
+++ apps/ngx-popovers/src/app/pages/landing/landing.component.ts
@@ -1,16 +1,6 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Inject,
-  inject,
-  NgZone,
-  PLATFORM_ID,
-  QueryList,
-  ViewChildren
-} from '@angular/core';
+import { AfterViewInit, Component, Inject, inject, NgZone, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { CommonModule, DOCUMENT, isPlatformServer } from '@angular/common';
-import anime from 'animejs';
+import anime, { AnimeInstance } from 'animejs';
 import { FormsModule } from '@angular/forms';
 import { CoreService } from '@demo/core/core.service';
 import { RouterLink } from '@angular/router';
@@ -22,14 +12,17 @@ import { RouterLink } from '@angular/router';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent implements AfterViewInit {
+export class LandingComponent implements AfterViewInit, OnDestroy {
   platformId = inject(PLATFORM_ID);
   isServer = () => isPlatformServer(this.platformId);
 
   title = 'ngx-popovers';
   description = 'Angular Floating components';
 
-  blocksCount = new Array(80).fill(0);
+  blocksCount = new Array(30).fill(0);
+
+  animations: AnimeInstance[] = [];
+  isDestroy = false;
 
   get blockWidth() {
     return (this.doc.documentElement.scrollWidth ?? 0) / 2;
@@ -58,8 +51,7 @@ export class LandingComponent implements AfterViewInit {
           duration: 300
         });
 
-        anime
-          .timeline()
+        anime.timeline()
           .add({
             targets: '.landing-heading',
             opacity: [0, 1],
@@ -76,7 +68,14 @@ export class LandingComponent implements AfterViewInit {
     }
   }
 
+  ngOnDestroy() {
+    this.isDestroy = true;
+  }
+
   animateBlocks = () => {
+    if (this.isDestroy) {
+      return;
+    }
     anime({
       targets: '.block',
       translateX: () => anime.random(-this.blockWidth, this.blockWidth),
@@ -91,7 +90,7 @@ export class LandingComponent implements AfterViewInit {
   };
 
   animateBlocksShadow = () => {
-    anime({
+    return anime({
       targets: '.block',
       boxShadow: '6px 9px 5vw 4px rgb(0 0 0 / 10%)',
       delay: anime.stagger(20),
@@ -99,8 +98,4 @@ export class LandingComponent implements AfterViewInit {
       easing: 'easeOutExpo'
     });
   };
-
-  htmlFromQueryList(list: QueryList<ElementRef<HTMLElement>>) {
-    return list.map(ref => ref.nativeElement);
-  }
 }
