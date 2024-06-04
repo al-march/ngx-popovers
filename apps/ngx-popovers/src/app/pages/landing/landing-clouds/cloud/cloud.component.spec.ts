5bb356d315fbe0fe965f0598426ff5ad6fa177fe
Alexandr <42899726+al-march@users.noreply.github.com>
2024-06-04 23:00:34 +0300

    feat(demo) new landing (#106)

---
+++ apps/ngx-popovers/src/app/pages/landing/landing-clouds/cloud/cloud.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CloudComponent } from './cloud.component';
+
+describe('CloudComponent', () => {
+  let component: CloudComponent;
+  let fixture: ComponentFixture<CloudComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CloudComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CloudComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/ngx-popovers/src/app/pages/landing/landing-clouds/landing-clouds.component.ts
@@ -0,0 +1,73 @@
+import { AfterViewInit, Component, ElementRef, signal, viewChildren } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CloudComponent } from '@demo/pages/landing/landing-clouds/cloud';
+import anime from 'animejs';
+
+function randomScale() {
+  return anime.random(60, 90) / 100;
+}
+
+@Component({
+  selector: 'ngx-clouds',
+  standalone: true,
+  imports: [CommonModule, CloudComponent],
+  templateUrl: './landing-clouds.component.html',
+  styleUrl: './landing-clouds.component.scss'
+})
+export class LandingCloudsComponent implements AfterViewInit {
+
+  clouds = signal([
+    {
+      scale: randomScale(),
+      top: 10,
+      left: 7
+    },
+    {
+      scale: randomScale(),
+      top: 2,
+      left: 90
+    },
+    {
+      scale: 0.7,
+      top: 32,
+      left: 40
+    },
+    {
+      scale: 0.4,
+      top: 50,
+      left: 58
+    },
+    {
+      scale: 0.6,
+      top: 12,
+      left: 32
+    }
+  ]);
+
+  cloudsList = viewChildren<ElementRef<HTMLElement>>('cloudItem');
+
+  ngAfterViewInit() {
+    const clouds = this.cloudsList().map(cloud => {
+      return cloud.nativeElement;
+    });
+
+    anime.timeline()
+      .add({
+        targets: clouds,
+        translateX: () => anime.random(-200, 200),
+        translateY: () => anime.random(-50, 250),
+        duration: 0
+      })
+      .add({
+        direction: 'alternate',
+        targets: clouds,
+        translateX: () => anime.random(-500, 500),
+        translateY: () => anime.random(-120, 120),
+        duration: () => anime.random(20_000, 45_000),
+        easing: 'linear',
+        delay: anime.stagger(20),
+        loop: true
+      });
+  }
+
+}
