f2393e072d9ad2e4bc14d239979b50f3d8b0a34d
Alexander <valverin95@gmail.com>
2024-06-05 20:42:10 +0300

    feat(demo) remove animejs from landing popover

--- apps/ngx-popovers/src/app/pages/landing/landing.component.ts
+++ apps/ngx-popovers/src/app/pages/landing/landing.component.ts
@@ -1,14 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  effect,
-  ElementRef,
-  inject,
-  model,
-  OnDestroy,
-  PLATFORM_ID,
-  viewChild
-} from '@angular/core';
+import { AfterViewInit, Component, inject, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformServer, NgOptimizedImage } from '@angular/common';
 import anime from 'animejs';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +7,6 @@ import { RouterLink } from '@angular/router';
 import { LandingCloudsComponent } from '@demo/pages/landing/landing-clouds';
 import { PopoverAnchor, PopoverComponent, PopoverTemplate } from '@ngx-popovers/popover';
 import { DocNavigationComponent } from '@demo/pages/documentation/components/doc-navigation';
-import { flip, offset } from '@floating-ui/dom';
 
 @Component({
   selector: 'ngx-popovers-landing',
@@ -43,38 +32,9 @@ export class LandingComponent implements AfterViewInit, OnDestroy {
   title = 'ngx-popovers';
   description = 'Angular Floating components';
 
-  popover = model(false);
-  popoverContent = viewChild<ElementRef<HTMLElement>>('popoverContent');
-
-  middleware = [
-    offset(5),
-    flip()
-  ];
-
   constructor(
     public core: CoreService
   ) {
-    effect(() => {
-      const isOpen = this.popover();
-      const target = this.popoverContent()?.nativeElement;
-
-      if (isOpen && target) {
-
-        anime.timeline()
-          .add({
-            targets: target,
-            translateY: '100%',
-            duration: 0,
-          })
-          .add({
-            targets: target,
-            translateY: '0',
-            easing: 'easeOutElastic(1, .6)',
-            duration: 400
-          });
-      }
-      console.log('open', isOpen);
-    });
   }
 
   ngAfterViewInit() {
