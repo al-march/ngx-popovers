f1655e2b4b8ed021acb6f9e42e6326f2d611e9f8
Alexandr <42899726+al-march@users.noreply.github.com>
2024-08-25 14:20:54 +0300

    feat(core) added strategy input to the floating component (#157)

--- packages/core/src/lib/floating/core/floating-config.ts
+++ packages/core/src/lib/floating/core/floating-config.ts
@@ -1,20 +1,22 @@
-import { MiddlewareList, offset, Placement } from '../../type';
-import { flip } from '@floating-ui/dom';
+import { flip, MiddlewareList, offset, Placement, Strategy } from '../../type';
 
 export interface FloatingConfig {
   placement: Placement;
   autoUpdate: boolean;
-  middleware: MiddlewareList;
   bindTo?: HTMLElement | string;
+  strategy?: Strategy;
+
+  middleware: MiddlewareList;
 }
 
 export class NgxFloatingConfig implements FloatingConfig {
   placement: Placement = 'bottom';
   autoUpdate = true;
   bindTo?: HTMLElement | string;
+  strategy?: Strategy;
   middleware: MiddlewareList = [
     offset(4),
-    flip(),
+    flip()
   ];
 
   constructor(
