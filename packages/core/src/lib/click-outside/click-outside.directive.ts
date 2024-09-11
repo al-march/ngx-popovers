bf9f75013fda3cd3f7015390def557aedcd9d286
Alexander <valverin95@gmail.com>
2024-09-11 21:06:33 +0300

    feat(core): clickOutside refactor

--- packages/core/src/lib/click-outside/click-outside.directive.ts
+++ packages/core/src/lib/click-outside/click-outside.directive.ts
@@ -1,11 +1,18 @@
 import { Directive, ElementRef, HostListener, output } from '@angular/core';
 import { isContainElement } from '../utils/utils';
 
+export interface ClickOutsideEvent {
+  readonly inside: boolean;
+  readonly outside: boolean;
+  readonly target: EventTarget;
+}
+
 @Directive({
   selector: '[ngxClickOutside]',
   standalone: true
 })
 export class ClickOutsideDirective {
+  ngxClickOutside = output<ClickOutsideEvent>();
   inside = output<EventTarget>();
   outside = output<EventTarget>();
 
@@ -20,8 +27,18 @@ export class ClickOutsideDirective {
     if (target) {
       if (isContainElement(this.el.nativeElement, target)) {
         this.inside.emit(target);
+        this.ngxClickOutside.emit({
+          inside: true,
+          outside: false,
+          target,
+        })
       } else {
         this.outside.emit(target);
+        this.ngxClickOutside.emit({
+          inside: false,
+          outside: true,
+          target,
+        })
       }
     }
   }
