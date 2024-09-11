acb3f709711334baae8b2de83275dae3e9923ca6
Alexandr <42899726+al-march@users.noreply.github.com>
2024-09-11 23:36:19 +0300

    feat(docs): update portal examples (#184)

---
+++ apps/ngx-popovers/src/app/pages/documentation/page-core/page-portal/examples/2/index.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PortalComponent } from "@ngx-popovers/core";
+
+@Component({
+  selector: 'demo-index',
+  standalone: true,
+  imports: [CommonModule, PortalComponent],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IndexComponent {
+  sections = [
+    'sect1',
+    'sect2',
+    'sect3',
+    'sect4'
+  ];
+
+  section = signal(this.sections[0]);
+  selector = computed(() => this.section() ? `#${this.section()} .section-portal` : '');
+}
