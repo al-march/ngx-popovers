05ce9f83d07f7b61f5b9985c1fd49147863b95d5
Alexander <valverin95@gmail.com>
2024-09-22 11:44:23 +0300

    feat(docs): page arrow docs

--- apps/ngx-popovers/src/app/pages/documentation/page-core/page-arrow/page-arrow.component.ts
+++ apps/ngx-popovers/src/app/pages/documentation/page-core/page-arrow/page-arrow.component.ts
@@ -1,59 +1,36 @@
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TitleComponent } from '@demo/pages/documentation/ui/components/title/title.component';
 import { HighlightComponent } from '@demo/core/highlight';
-
-const defaultUsage = `
-<div #trigger>trigger</div>
-
-<ngx-floating
-  [reference]="trigger"
-  placement="bottom"
-  bindTo=".body"
->
-  <p>Floating content</p>
-  
-  <ngx-arrow padding="20" />
-</ngx-floating>
-`.trim();
-
-const configExample = `
-import { Component, Provider } from '@angular/core';
-import { ArrowBase, NGX_ARROW_COMPONENT } from '@ngx-popovers/core';
-
-@Component({
-  standalone: true,
-  template: \`
-    <div
-      style="
-        width: 12px;
-        height: 12px;
-        transform: rotate(45deg);
-      "
-      class="bg-primary"
-    ></div>
-  \`
-})
-export class CustomArrow extends ArrowBase {
-}
-
-export const ArrowProvider: Provider = {
-  provide: NGX_ARROW_COMPONENT,
-  useValue: CustomArrow
-};
-`.trim();
+import { TitleComponent } from '@demo/pages/documentation/ui/components/title/title.component';
+import { CodeExampleTabsComponent } from "../../ui/components/code-example-tabs/code-example-tabs.component";
+import { DocPageComponent } from "../../ui/components/doc-page/doc-page.component";
+import { DocSectionComponent } from "../../ui/components/doc-page/doc-section/doc-section.component";
+import { SubTitleComponent } from "../../ui/components/doc-page/sub-title/sub-title.component";
 
 @Component({
   selector: 'ngx-popovers-page-arrow',
   standalone: true,
   imports: [
+    CommonModule,
     TitleComponent,
-    HighlightComponent
-  ],
+    HighlightComponent,
+    DocPageComponent,
+    DocSectionComponent,
+    SubTitleComponent,
+    CodeExampleTabsComponent,
+    AsyncPipe
+],
   templateUrl: './page-arrow.component.html',
   styleUrl: './page-arrow.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageArrowComponent {
-  defaultUsage = defaultUsage;
-  configExample = configExample;
+  example1 = {
+    EXAMPLE: import('./examples/1/index.component').then(m => m.IndexComponent),
+    HTML: import('./examples/1/index.component.html?raw').then(m => m.default),
+    TS: import('./examples/1/index.component.ts?raw').then(m => m.default),
+    SCSS: import('./examples/1/index.component.scss?raw').then(m => m.default)
+  };
+
+  configExample = import('./examples/2/index.config.ts?raw').then(m => m.default);
 }
