ee67fffb0ef328aef6ccae3420a4007be1e3e4c3
Alexandr <42899726+al-march@users.noreply.github.com>
2025-03-09 11:24:20 +0300

    fix(core): fix placement type for idea (#224)

--- packages/core/src/lib/type.ts
+++ packages/core/src/lib/type.ts
@@ -1,5 +1,7 @@
 import { Middleware } from '@floating-ui/dom';
+import { AlignedPlacement, Side } from '@floating-ui/utils';
 
 export * from '@floating-ui/dom';
 
+export type Placement = Side | AlignedPlacement
 export type MiddlewareList = Array<Middleware | null | undefined | false>;
